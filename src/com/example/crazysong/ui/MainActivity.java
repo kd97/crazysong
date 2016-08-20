47aa424acce5ef93ac8147a906d3e5fe6dd7a821
iverson <13895770190@163.com>
2016-08-20 22:19:18 +0800

    First commit.

---
+++ src/com/example/crazysong/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.crazysong.ui;
+
+import com.example.crazysong.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+   
+}
